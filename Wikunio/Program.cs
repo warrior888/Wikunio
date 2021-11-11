774e4930576894a29b086a4a0e2f0288d76150c7
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-11-11 17:10:56 +0100

    Add project files.

---
+++ Wikunio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Wikunio
+{
+    class Program
+    {
+        static void Main(string[] args) //zielona flaga w scratch
+        {
+            string wejscieZKonsoli = "";
+                
+            Console.WriteLine("Wikunio uczy sie programowania w c#!");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            while (true) //zawsze w scratch
+            {
+                Console.WriteLine("Podaj dowolne dane: ");
+                wejscieZKonsoli = Console.ReadLine();
+
+                if (wejscieZKonsoli.Contains("-04"))
+                {
+                    Console.WriteLine("Kwiecien");
+                }
+
+                if (wejscieZKonsoli.Contains("-02"))
+                {
+                    Console.WriteLine("Luty");
+                }
+            }
+        }
+    }
+}
